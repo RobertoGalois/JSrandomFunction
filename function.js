68f8bdde56a117a5b1437d34873945b6ecdf021d
RobertoGalois <victor.vague@live.fr>
2017-05-16 13:12:27 +0200

    Update function.js

--- function.js
+++ function.js
@@ -2,7 +2,7 @@ function rand(pMin, pMax, pInteger) {
 	if(pInteger)
 		return Math.floor(((Math.random() * (pMax-pMin+1)) + pMin));		
 	else
-    return ((Math.random() * (pMax-pMin)) + pMin);
+    		return ((Math.random() * (pMax-pMin)) + pMin);
 }
 
 //same but better way to do
