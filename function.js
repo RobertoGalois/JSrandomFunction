5ff9c5ea39da9094d70af4baeadc62e425a44ba7
RobertoGalois <victor.vague@live.fr>
2017-05-16 13:11:33 +0200

    Create function.js

---
+++ function.js
@@ -0,0 +1,9 @@
+function rand(pMin, pMax, pInteger) {
+	if(pInteger)
+		return Math.floor(((Math.random() * (pMax-pMin+1)) + pMin));		
+	else
+    return ((Math.random() * (pMax-pMin)) + pMin);
+}
+
+//same but better way to do
+const rand2 = (pMin, pMax, pInteger) => (pInteger)?Math.floor(((Math.random() * (pMax-pMin+1)) + pMin)):((Math.random() * (pMax-pMin)) + pMin);
